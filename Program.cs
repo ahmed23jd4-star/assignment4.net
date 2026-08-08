6dd39815efbdad4b3c57837549d4c890cf55df92
ahmed23jd4-star <ahmed23jd4@gmail.com>
2026-08-08 15:04:56 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region q1
+            //Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print
+            //the second price(index 1).
+
+            //double[] prices = { 25.5, 40, 33.75 };
+            //Console.WriteLine(prices[1]);
+
+            #endregion
+
         }
     }
 }
