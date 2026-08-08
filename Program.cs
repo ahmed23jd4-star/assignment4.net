2926bede2cef013b491656878d22d31b8554ac43
ahmed23jd4-star <ahmed23jd4@gmail.com>
2026-08-08 15:01:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
