53a6821897da7a173ae7a43944de8a44711c62df
ahmed23jd4-star <ahmed23jd4@gmail.com>
2026-08-08 15:11:48 +0300

    Q2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace assignment4
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace assignment4
 {
     internal class Program
     {
@@ -10,6 +12,18 @@
 
             //double[] prices = { 25.5, 40, 33.75 };
             //Console.WriteLine(prices[1]);
+
+            #endregion
+            #region q2
+            //Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+            //shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+
+            //int [,] shelfcopies = { { 3 , 5} , {1 ,4}  } ;
+            //Console.WriteLine(shelfcopies[ 1 ,0]);
+            //foreach (var item in shelfcopies)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
             #endregion
 
